23e920584bca44fadb01d4b47d6224e6c295503f
endaaman <endaaman@eis.hokudai.ac.jp>
2016-10-18 01:19:05 +0900

    add user

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
+
+class User < ApplicationRecord
+  has_secure_password
+
+  before_create do
+    self
+  end
+end
