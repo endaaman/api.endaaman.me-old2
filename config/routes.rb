34f22aadf5bf4a94ca53de70ba3b367925b425e1
endaaman <endaaman@eis.hokudai.ac.jp>
2016-10-22 04:01:25 +0900

    do not use resourceful rouring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  resources :memos
-  resources :users
-  resources :files
+  constraints filename: /.*/ do
+  # constraints format: // do
+  # scope format: false do
+    namespace :memos do
+      get '/', action: 'index'
+      post '/', action: 'create'
+      patch '/:id', action: 'update'
+      delete '/:id', action: 'delete'
+    end
+
+    namespace :users do
+      get '/', action: 'index'
+    end
+
+    namespace :files do
+      get '/', action: 'index'
+      post '/', action: 'create'
+      post '/rename', action: 'rename'
+      delete '/:filename', action: 'delete'
+    end
+  end
+
+  # resources :files, param: :filename
   resource :session
 end
